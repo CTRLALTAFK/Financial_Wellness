1f1b72528a63b5332442c7d836b32b0fc72ba48a
Pete Farquhar <petefarq@gmail.com>
2020-01-05 11:12:33 -0500

    Pete data changes 1-5-20

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,85 +1,129 @@
-// Initialize SVG Chart 1
-
-var svgWidth = 700;
-var svgHeight = 500;
-
-var margin = {
-  top: 20,
-  right: 40,
-  bottom: 20,
-  left: 50
-};
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
 
 // Load data
-d3.csv("/static/data/housing.csv").then(function(housing_data) {
+d3.csv("/static/data/housing.csv").then(d => chart(d))
 
-	console.log(housing_data);
-	document.getElementById("test1").innerHTML = housing_data;
-  
+function chart(csv) {
 
-    var stack = d3.stack()
-	  .keys(['Never', 'Sometimes', 'Often'])
-	//   .value(function(d) { return d.values.value; })
+	var keys = csv.columns.slice(2);
 
-    var series = stack(housing_data);
+	var category   = [...new Set(csv.map(d => d.Category))]
 
-    console.log(series)
+	var options = d3.select("#category").selectAll("option")
+		.data(category)
+	.enter().append("option")
+		.text(d => d)
 
-   			//Set up scales
-			var xScale = d3.scaleBand()
-				.domain(d3.range(series.length))
-				.range([0, width])
-				.paddingInner(0.05);
-		
-			var yScale = d3.scaleLinear()
-				.domain([0,				
-					d3.max(series, function(d) {
-						return 100;
-					})
-				])
-				.range([height, 0]);  // <-- Flipped vertical scale
-				
-			//Easy colors accessible via a 10-step ordinal scale
-			var colors = d3.scaleOrdinal(d3.schemeCategory10);
-    
-      // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-      var svg = d3.select("#chart1")
-      .append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
+	var svg = d3.select("#chart"),
+		margin = {top: 35, left: 35, bottom: 0, right: 0},
+		width = +svg.attr("width") - margin.left - margin.right,
+		height = +svg.attr("height") - margin.top - margin.bottom;
 
-      // Add a group for each row of data
-			var groups = svg.selectAll("g")
-				.data(series)
-				.enter()
-				.append("g")
-				.style("fill", function(d, i) {
-					return colors(i);
-				});
+	var x = d3.scaleBand()
+		.range([margin.left, width - margin.right])
+		.padding(0.1)
+
+	var y = d3.scaleLinear()
+		.rangeRound([height - margin.bottom, margin.top])
+
+	var xAxis = svg.append("g")
+		.attr("transform", `translate(0,${height - margin.bottom})`)
+		.attr("class", "x-axis")
+
+	var yAxis = svg.append("g")
+		.attr("transform", `translate(${margin.left},0)`)
+		.attr("class", "y-axis")
+
+	var z = d3.scaleOrdinal()
+		.range(["steelblue", "darkorange", "lightblue"])
+		.domain(keys);
+
+	update(d3.select("#category").property("value"), 0)
+
+	function update(input, speed) {
+
+		var data = csv.filter(f => f.Category == input)
+
+		data.forEach(function(d) {
+			d.total = d3.sum(keys, k => +d[k])
+			return d
+		})
+
+		y.domain([0, d3.max(data, d => d3.sum(keys, k => +d[k]))]).nice();
+
+		svg.selectAll(".y-axis").transition().duration(speed)
+			.call(d3.axisLeft(y).ticks(null, "s"))
+
+		x.domain(data.map(d => d.Group));
+
+		svg.selectAll(".x-axis").transition().duration(speed)
+			.call(d3.axisBottom(x).tickSizeOuter(0))
+
+		var group = svg.selectAll("g.layer")
+			.data(d3.stack().keys(keys)(data), d => d.key)
+
+		group.exit().remove()
+
+		group.enter().append("g")
+			.classed("layer", true)
+			.attr("fill", d => z(d.key));
+
+		var bars = svg.selectAll("g.layer").selectAll("rect")
+			.data(d => d, e => e.data.Group);
+
+		bars.exit().remove()
+
+		bars.enter().append("rect")
+			.attr("width", x.bandwidth())
+			.merge(bars)
+		.transition().duration(speed)
+			.attr("x", d => x(d.data.Group))
+			.attr("y", d => y(d[1]))
+			.attr("height", d => y(d[0]) - y(d[1]))
+
+		var text = svg.selectAll(".text")
+			.data(data, d => d.Group);
+
+		text.exit().remove()
+
+		text.enter().append("text")
+			.attr("class", "text")
+			.attr("text-anchor", "middle")
+			.merge(text)
+		.transition().duration(speed)
+			.attr("x", d => x(d.Group) + x.bandwidth() / 2)
+			.attr("y", d => y(d.total) - 5)
+			
+		var legend = svg.selectAll(".legend")
+			.data(keys)
+			.enter().append("g")
+			.attr("class", "legend")
+			.attr("transform", (d, i) => `translate(${margin.left},${i * 30})`);
 	
-			// Add a rect for each data value
-			var rects = groups.selectAll("rect")
-				.data(function(d) { return d; })
-				.enter()
-				.append("rect")
-				.attr("x", function(d, i) {
-					return xScale(i);
-				})
-				.attr("y", function(d) {
-					return yScale(d[1]);  // <-- Changed y value
-				})
-				.attr("height", function(d) {
-					return yScale(d[0]) - yScale(d[1]);  // <-- Changed height value
-				})
-				.attr("width", xScale.bandwidth());
-    
-  }).catch(function(error) {
-    console.log(error);
-  });
+		legend.append("text")
+			.attr("x", width - 10)
+			.attr("y", margin.top)
+			.attr("text-anchor", "end")
+			.attr("dy", "1em")
+			.text(d => d);
+	
+		legend.append("rect")
+			.attr("width", 19)
+			.attr("height", 19)
+			.attr("x", width)
+			.attr("y", margin.top)
+			.attr("fill", z)
+			.attr("stroke", z)
+			.attr("value", d => d)
+	
+	
+		}
+
+	var select = d3.select("#category")
+		.on("change", function() {
+			update(this.value, 750)
+		})
+
+}
 
 
 
